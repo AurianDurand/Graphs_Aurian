1c5507704cee19f2357fa430bcb2eba125151de5
Aurian Durand <34919475+AurianDurand@users.noreply.github.com>
2018-06-19 14:07:52 +0200

    fdhgsdfdwxc

--- CryptOfTheJavaDancer/src/cryptofthejavadancer/Model/IA/IA_Diamants.java
+++ CryptOfTheJavaDancer/src/cryptofthejavadancer/Model/IA/IA_Diamants.java
@@ -21,28 +21,36 @@ public class IA_Diamants extends IA {
 
     @Override
     public Type_Action action() {
+        Vertex Cadence = getMap().getGraphe_simple().getVertex(super.getEntite().getCase().getLigne()+"/"+super.getEntite().getCase().getColonne());
         Type_Action toDo = Type_Action.attendre;
         if(firstTime == true){
             firstTime = false;
             algorithme = new Dijkstra(this.getMap().getGraphe_simple());
-            //algorithme.calcul(getMap().getGraphe_simple().getVertex(getMap().getDepart().toString()),getMap().getGraphe_simple().getVertex(getMap().getSortie().toString()));
         }
+
         //if cadence hasn't anything to do
         if(algorithme.getPath().isEmpty()){
-            Vertex Cadence = getMap().getGraphe_simple().getVertex(super.getEntite().getCase().getLigne()+"/"+super.getEntite().getCase().getColonne());
-            //if cadence is on a diamant
+            //algorithme.calcul(getMap().getGraphe_simple().getVertex(getMap().getDepart().toString()), getMap().getGraphe_simple().getVertex(getMap().getSortie().toString()));
+            Cadence = getMap().getGraphe_simple().getVertex(super.getEntite().getCase().getLigne()+"/"+super.getEntite().getCase().getColonne());
+            algorithme.calcul(Cadence, getMap().getGraphe_simple().getVertex(getMap().getSortie().toString()));
+
             if(closerDiamant()!=null){
+                //System.out.println("closer diamant : "+closerDiamant().getCoordinates());
+                //if cadence is on a diamant
                 if(closerDiamant()==Cadence){
                     toDo = Type_Action.ramasser;
+                }else{
+                    algorithme.calcul(Cadence,closerDiamant());
                 }
-                //if cadence is on the exit
+
+            //if cadence is on the exit
             }else if(Cadence==getMap().getGraphe_simple().getVertex(getMap().getSortie().toString())){
                 toDo = Type_Action.sortir;
-            }else {
-                algorithme.calcul(getMap().getGraphe_simple().getVertex(getMap().getDepart().toString()), getMap().getGraphe_simple().getVertex(getMap().getSortie().toString()));
+            }else{
                 toDo = vertexToAction(getMap().getCase(algorithme.getPath().get(0).getCase().getLigne(), algorithme.getPath().get(0).getCase().getColonne()));
             }
         }else{
+            System.out.println("Cadence : "+Cadence.getCoordinates());
             toDo = vertexToAction(getMap().getCase(algorithme.getPath().get(0).getCase().getLigne(),algorithme.getPath().get(0).getCase().getColonne()));
         }
         return toDo;
@@ -53,24 +61,11 @@ public class IA_Diamants extends IA {
         Vertex closerOne = null;
         for(Objet obj : this.getMap().getListeObjet()){
             if(obj.getType()== Type_Objet.Diamant){
-
-                Vertex Cadence = getMap().getGraphe_simple().getVertex(super.getEntite().getCase().getLigne()+"/"+super.getEntite().getCase().getColonne());
-
-                Vertex start = getMap().getGraphe_simple().getVertex(getMap().getDepart().toString());
                 Vertex diamant = this.getMap().getGraphe_simple().getVertex(obj.getCase().getLigne()+"/"+obj.getCase().getColonne());
-                //algorithme.calcul(start,diamant);
-
                 if(algorithme.getDistance(diamant)<dist){
                     dist = algorithme.getDistance(diamant);
                     closerOne = diamant;
                 }
-
-
-
-                /*if(algorithme.getPath().size()<dist){
-                    dist = algorithme.getPath().size();
-                    closerOne = diamant;
-                }*/
             }
         }
         return closerOne;
@@ -134,7 +129,7 @@ public class IA_Diamants extends IA {
                 toDo = Type_Action.interagir_droite;
             }
         }
-        System.out.println("toDo : "+toDo);
+        //System.out.println("toDo : "+toDo);
         return toDo;
     }
 
